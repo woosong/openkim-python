ef5c18df67eaf292bd3d92749d5f6a48e6562ac3
Woosong Choi <woosong@bawi.org>
2011-12-13 18:23:51 -0500

    better memory control (added free object)

--- neighborlist.h
+++ neighborlist.h
@@ -1,6 +1,7 @@
 #ifndef _NEIGHBORLIST_H_
 #define _NEIGHBORLIST_H_
 int set_neigh_object(void* kimmdl, int sz1, int* NNeighbors, int sz2, int* neighborList, int sz3, double* RijList);  
+int free_neigh_object(void* kimmdl); 
 int set_kim_periodic_full_neigh(void* kimmdl); 
 int set_kim_periodic_half_neigh(void* kimmdl); 
 int set_kim_cluster_full_neigh(void* kimmdl); 
