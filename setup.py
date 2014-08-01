a4215c90a35fba9aec3450bb52efcd3833f3e2a6
Matt Bierbaum <matt.bierbaum@gmail.com>
2014-08-01 18:22:52 +0000

    new build system, using the KIM build config util and setup.cfg

--- setup.py
+++ setup.py
@@ -3,29 +3,86 @@
 setup.py file for SWIG kimservice
 """
 
+from __future__ import print_function
 from numpy.distutils.core import setup, Extension
+from distutils.spawn import find_executable
+from subprocess import check_output
 import os
+import sys
 
-kim_dir = os.environ['KIM_DIR']
-kim_api_dir = kim_dir + 'KIM_API/'
+try:
+    import configparser
+except:
+    import ConfigParser as configparser
+
+def run_kim_build_config(opt='--version'):
+    buildconf = find_executable("kim-api-build-config")
+    if not buildconf:
+        print("warning: kim-api-build-config not found on PATH while search for '%s'" % opt)
+        return
+
+    return check_output([buildconf, opt])
+
+def getdefault(conf, sect, key, default=None):
+    try:
+        conf.get(sect, key)
+    except configparser.NoSectionError as e:
+        return default
+    except configparser.NoOptionError as e:
+        return default
+
+def default_kim_dir():
+    default = '/usr/local/lib/kim-api/'
+    return (
+        os.environ.get("KIM_DIR", None) or
+        (default if os.path.exists(default) else '') or
+        os.path.join(os.environ.get("HOME", None), 'openkim-api')
+    )
+
+def default_include():
+    libdir = (
+        run_kim_build_config('--includes') or
+        '-I'+os.path.join(default_kim_dir(), 'include') or ''
+    )
+    return [ a.replace('-I', '') for a in libdir.split(' ') if a.startswith('-I') ]
+
+def default_libdir():
+    libdir = (
+        run_kim_build_config('--ldflags') or
+        '-L'+os.path.join(default_kim_dir(), '') or ''
+    )
+    return [ a.replace('-L', '') for a in libdir.split(' ') if a.startswith('-L') ]
+
+def default_libs():
+    liblist = run_kim_build_config('--ldlibs') or "-lkim -lgfortran"
+    return [ a.strip() for a in liblist.replace('-l', '').split(' ') ]
+
+options = {}
+conf = configparser.SafeConfigParser()
+setup_cfg = 'setup.cfg' if os.path.exists('setup.cfg') else 'setup.cfg.default'
+conf.read(setup_cfg)
+
+options['include'] = getdefault(conf, 'build', 'include') or default_include()
+options['libdir'] = getdefault(conf, 'build', 'libdir') or default_libdir()
+options['libs'] = getdefault(conf, 'build', 'libs') or default_libs()
 
 kimservice_module = Extension('_kimservice',
     sources=['kimservice.i'],
     swig_opts=['-c++'],
-    include_dirs=[kim_api_dir],
-    library_dirs=[kim_api_dir],
-    libraries=['kim','gfortran']
+    include_dirs=options['include'],
+    library_dirs=options['libdir'],
+    libraries=options['libs']
     )
 
 kimneighborlist_module = Extension('_kimneighborlist',
     sources=['neighborlist.i','neighborlist.c','cvec.c'],
-    include_dirs=[kim_api_dir, '.'],
-    library_dirs=[kim_api_dir],
-    libraries=['kim']
+    include_dirs=options['include']+['.'],
+    library_dirs=options['libdir'],
+    libraries=options['libs']
     )
 
 setup (name = 'kimservice',
-    version = '0.0.1a',
+    version = '0.9.0',
     author      = "Woosong Choi, Matt Bierbaum, Yanjiun Chen",
     description = """KIM python interface""",
     ext_modules = [kimservice_module, kimneighborlist_module],
